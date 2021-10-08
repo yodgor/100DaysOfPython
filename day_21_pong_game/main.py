cf957dcd0597423009cddb633b77ef86380c56bc
Yodgorbek Shukhratbekov <32003637+yodgor@users.noreply.github.com>
2021-10-08 14:52:49 -0400

    final_code

--- day_21_pong_game/main.py
+++ day_21_pong_game/main.py
@@ -2,6 +2,7 @@ import time
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.bgcolor("black")
@@ -13,6 +14,7 @@ l_paddle = Paddle((-350,0))
 
 ball=Ball()
 
+scoreboard=Scoreboard()
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
@@ -21,9 +23,23 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
-    ball.move(
+    ball.move()
+    scoreboard.update_scoreboard()
     #Detect collision with the wall
-     
+    if ball.ycor()>280 or ball.ycor()<-280:
+        #neeed to bounce
+        ball.bounce_y()
+    # Detect collision with paddle
+    if ball.distance(r_paddle)<50 and ball.xcor()>320 or ball.distance(l_paddle)<50 and ball.xcor()<-320:
+        ball.bounce_x()
+    #Detect R paddle misses
+    if ball.xcor() >380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() <-380:
+        ball.reset_position()
+        scoreboard.r_point()
 screen.exitonclick()
