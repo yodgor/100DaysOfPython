067655f58d3b5a1dfa374980fdfcf7e3cfac8d10
Yodgorbek Shukhratbekov <32003637+yodgor@users.noreply.github.com>
2021-10-22 17:10:06 -0400

    Add_button_function

--- day_29_password_generator/main.py
+++ day_29_password_generator/main.py
@@ -4,6 +4,16 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password}")
+
+
+    return 0
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -28,13 +38,14 @@ website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2)
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "shuhratbekov0612@gmail.com")
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 #Buttons
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1,columnspan=2)
 
 
