acc508e562e6617d64d4f2758b3c3866d73ab41d
Yodgorbek Shukhratbekov <32003637+yodgor@users.noreply.github.com>
2021-10-31 15:55:09 -0400

    functions added

--- day_31_flashcards/main.py
+++ day_31_flashcards/main.py
@@ -1,2 +1,52 @@
-BACKGROUND_COLOR = "#B1DDC6"
+from tkinter import *
+import pandas
+import random
+data = pandas.read_csv("data/french_words.csv")
 
+to_learn = data.to_dict(orient="records")
+
+current_card ={}
+
+
+def next_card():
+    global current_card
+    current_card = random.choice(to_learn)
+    #print(current_card["French"])
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="back")
+    canvas.itemconfig(card_background, image=card_front_img)
+def flip_card():
+    canvas.itemconfig(card_title,text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
+
+BACKGROUND_COLOR = "#B1DDC6"
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+window.after(3000, func=flip_card)
+
+
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
+
+
+# <editor-fold desc="Description">
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
+unknown_button.grid(row=1, column=0)
+
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(image=check_image, highlightthickness=0, command=next_card)
+known_button.grid(row=1, column=1)
+#</editor-fold>
+
+next_card()
+
+window.mainloop()
